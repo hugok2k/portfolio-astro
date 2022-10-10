7526ce26639265bbc33042226c5ae87ee1cdf1fb
Hugo Avila <hugok2k@gmail.com>
2022-10-10 20:40:36 -0300

    update project and src images

---
+++ src/components/utils/data.js
@@ -0,0 +1,66 @@
+export const data = [
+  {
+    href: 'https://github.com/hugok2k/portfolio-astro',
+    title: 'Porfolio Astro',
+    body: 'Portfolio personal realizado en Astro optimizado para una rápida apertura y baja transferencia.',
+    image: '/images/portfolio.jpg',
+    tools: 'Astro - JS',
+    deploy: 'https://hugoavila.vercel.app/'
+  },
+  {
+    href: 'https://github.com/PF-Musicfy/app-Musicfy',
+    title: 'Musicfy APP',
+    body: 'Proyecto grupal final en Henry, bootcamp full-stack. Aplicación web streaming de música.',
+    image: '/images/musicfy.jpg',
+    tools: 'ReactJS - NodeJS - MongoDB - CSS',
+    deploy: 'https://app-musicfy.vercel.app/'
+  },
+  {
+    href: 'https://github.com/hugok2k/PI-Henry-Food',
+    title: 'Food API',
+    body: 'Proyecto individual en Henry, bootcamp full-stack. Sitio web de recetas utilizando API de tercero.',
+    image: 'images/pihenry.jpg',
+    tools: 'ReactJS - NodeJS - PostgreSQL - CSS',
+    deploy: 'https://pi-henry-food-phi.vercel.app/'
+  },
+  {
+    href: 'https://github.com/hugok2k/python-ahorcado',
+    title: 'Juego Ahorcado',
+    body: 'Juego realizado en Python para jugarlo en consola',
+    image: '/images/ahorcado.jpg',
+    tools: 'PYTHON',
+    deploy: 'https://replit.com/@hugok2k/ahorcado'
+  },
+  {
+    href: 'https://github.com/hugok2k/Python-ppt',
+    title: 'Juego piedra, papel y tijera.',
+    body: 'Juego realizado en Python para jugarlo en consola',
+    image: '/images/ppt.jpg',
+    tools: 'PYTHON',
+    deploy: 'https://replit.com/@hugok2k/ppt'
+  },
+  {
+    href: 'https://github.com/hugok2k/CSS-ZeldaTriforceMarks',
+    title: 'Zelda Triforce Mark',
+    body: 'Dibujo realizado en puro CSS',
+    image: '/images/zeldatriforce.jpg',
+    tools: 'HTML - CSS',
+    deploy: 'https://hugok2k.github.io/CSS-ZeldaTriforceMarks/'
+  },
+  {
+    href: 'https://github.com/hugok2k/CSS-ZeldaTriforce',
+    title: 'Zelda Triforce Emblem',
+    body: 'Dibujo realizado en puro CSS',
+    image: '/images/zeldatriforcemarks.jpg',
+    tools: 'HTML - CSS',
+    deploy: 'https://hugok2k.github.io/CSS-ZeldaTriforce/'
+  },
+  {
+    href: 'https://github.com/hugok2k/CSS-ZeldaSheikah',
+    title: 'Zelda Emblema Sheikah',
+    body: 'Dibujo realizado en puro CSS',
+    image: '/images/zeldasheikah.jpg',
+    tools: 'HTML - CSS',
+    deploy: 'https://hugok2k.github.io/CSS-ZeldaSheikah/'
+  }
+]
