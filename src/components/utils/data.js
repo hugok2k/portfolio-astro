f4399c9aea15a175c0ae640aa1fa25daa6fc1439
Hugo Avila <hugok2k@gmail.com>
2023-03-29 12:56:14 -0300

    Fix data writing

--- src/components/utils/data.js
+++ src/components/utils/data.js
@@ -2,7 +2,7 @@ export const data = [
   {
     href: 'https://github.com/hugok2k/',
     title: 'Active Rent a Car',
-    body: 'Sitio web de alquiler de automóviles con posibilidad de modificar los precios en tiempo real.',
+    body: 'Sitio web de alquiler de automóviles con calendario de reserva. El usuario puede agregar, modificar o eliminar vehículos.',
     image: '/images/active.webp',
     tools: ['NextJS', 'TypeScript', 'CSS', 'GoogleAPI'],
     deploy: 'https://www.activerentacar.com.ar/'
@@ -10,7 +10,7 @@ export const data = [
   {
     href: 'https://github.com/hugok2k/cloudinary-hackaton',
     title: 'CustomAvatar',
-    body: 'Proyecto realizado para la hackaton de Cloudinary. Crea avatares detectando el rostro con IA Cloudinary.',
+    body: 'Proyecto realizado para la hackathon de Cloudinary. Crea avatares detectando el rostro con IA Cloudinary.',
     image: '/images/customavatar.webp',
     tools: ['NextJS', 'Tailwind'],
     deploy: 'https://cloudinary-hackaton.vercel.app/'
@@ -18,7 +18,7 @@ export const data = [
   {
     href: 'https://github.com/hugok2k/',
     title: '123 Auto Rental',
-    body: 'Sitio web de alquiler de automóviles con posibilidad de modificar los precios en tiempo real.',
+    body: 'Sitio web de alquiler de automóviles con calendario de reserva. El usuario puede agregar, modificar o eliminar vehículos.',
     image: '/images/123autorental.webp',
     tools: ['NextJS', 'CSS', 'GoogleAPI'],
     deploy: 'https://www.123autorental.com.ar/'
@@ -34,7 +34,7 @@ export const data = [
   {
     href: 'https://github.com/hugok2k/hacktoberfest-2022',
     title: 'Password Generator',
-    body: 'Generador de contraseñas seguras aleatorias utilizando letras, numeros, simbolos o bien palabras',
+    body: 'Generador de contraseñas aleatorias utilizando letras, números, símbolos o bien palabras.',
     image: '/images/passgen.webp',
     tools: ['Astro', 'ReactJS', 'CSS'],
     deploy: 'https://hacktoberfest-2022.vercel.app/entry/hugok2k'
@@ -42,7 +42,7 @@ export const data = [
   {
     href: 'https://github.com/PF-Musicfy/app-Musicfy',
     title: 'Musicfy APP',
-    body: 'Proyecto grupal final en Henry, bootcamp full-stack. Aplicación web streaming de música.',
+    body: 'Proyecto grupal final en Henry, bootcamp full-stack. Aplicación web streaming de música. Metodología SCRUM.',
     image: '/images/musicfy.webp',
     tools: ['ReactJS', 'NodeJS', 'MongoDB', 'CSS'],
     deploy: 'https://app-musicfy.vercel.app/'
@@ -56,31 +56,3 @@ export const data = [
     deploy: 'https://codepen.io/hugok2k'
   }
 ]
-
-// export const data = [
-
-//   {
-//     href: 'https://github.com/hugok2k/CSS-ZeldaTriforceMarks',
-//     title: 'Zelda Triforce Mark',
-//     body: 'Dibujo realizado en CSS puro',
-//     image: '/images/zeldatriforce.jpg',
-//     tools: 'HTML - CSS',
-//     deploy: 'https://hugok2k.github.io/CSS-ZeldaTriforceMarks/'
-//   },
-//   {
-//     href: 'https://github.com/hugok2k/CSS-ZeldaTriforce',
-//     title: 'Zelda Triforce Emblem',
-//     body: 'Dibujo realizado en CSS puro',
-//     image: '/images/zeldatriforcemarks.jpg',
-//     tools: 'HTML - CSS',
-//     deploy: 'https://hugok2k.github.io/CSS-ZeldaTriforce/'
-//   },
-//   {
-//     href: 'https://github.com/hugok2k/CSS-ZeldaSheikah',
-//     title: 'Zelda Emblema Sheikah',
-//     body: 'Dibujo realizado en CSS puro',
-//     image: '/images/zeldasheikah.jpg',
-//     tools: 'HTML - CSS',
-//     deploy: 'https://hugok2k.github.io/CSS-ZeldaSheikah/'
-//   }
-// ]
