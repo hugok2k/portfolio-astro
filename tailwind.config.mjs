be9493c72562c0b29d999535d6638932f55fcd29
Hugo Avila <hugok2k@gmail.com>
2025-06-12 14:49:54 -0300

    refactor: Enhance Tailwind CSS configuration by organizing theme colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: "#db2979",
+        secondary: "#ededed",
+        tertiary: "#ff6dae",
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 }
