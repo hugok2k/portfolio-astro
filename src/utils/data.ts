5ce9429919cc10557abc34d7fe8afdc20ef6cba3
Hugo Avila <hugok2k@gmail.com>
2025-06-22 16:47:45 -0300

    feat: Update project data structure to replace href with github and enhance descriptions

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,64 +1,72 @@
 export const data = [
   {
-    href: "",
+    github: "",
     title: "Farox - Landing Page",
-    body: "Desarrollada en colaboración con TintaSur para un diseño atractivo y moderno. Implementada con Next.js, i18n para multilenguaje, Tailwind para estilos y animaciones personalizadas. Una combinación de rendimiento y experiencia de usuario.",
+    description:
+      "Desarrollada en colaboración con TintaSur para un diseño atractivo y moderno. Implementada con Next.js, i18n para multilenguaje, Tailwind para estilos y animaciones personalizadas. Una combinación de rendimiento y experiencia de usuario.",
     image: "/images/faroxlanding.webp",
     tools: ["NextJS", "Tailwind"],
     deploy: "https://farox-landing.vercel.app/",
   },
   {
-    href: "",
+    github: "",
     title: "PC Soluciones Ituzaingó",
-    body: "Landing page desarrollada con Astro y Tailwind, enfocada en servicios de reparación y mantenimiento informático. Elegí Astro por su rendimiento y generación de sitios estáticos, ideal para una experiencia rápida y optimizada.",
+    description:
+      "Landing page desarrollada con Astro y Tailwind, enfocada en servicios de reparación y mantenimiento informático. Elegí Astro por su rendimiento y generación de sitios estáticos, ideal para una experiencia rápida y optimizada.",
     image: "/images/pcsoluciones.webp",
     tools: ["Astro", "Tailwind"],
     deploy: "https://pcsoluciones-ituzaingo.vercel.app/",
   },
   {
-    href: "",
+    github: "",
     title: "Psinfronteras",
-    body: "Landing page optimizada para campañas de Google Ads, diseñada para Psinfronteras, un servicio de psicología y psicoterapia online. Su enfoque está en la captación de leads y la presentación clara de los servicios.",
+    description:
+      "Landing page optimizada para campañas de Google Ads, diseñada para Psinfronteras, un servicio de psicología y psicoterapia online. Su enfoque está en la captación de leads y la presentación clara de los servicios.",
     image: "/images/psinfronteras.webp",
     tools: ["Astro", "Tailwind"],
     deploy: "https://psinfrontera-landing.vercel.app/",
   },
   {
-    href: "https://github.com/hugok2k/qrcode-generator",
+    github: "https://github.com/hugok2k/qrcode-generator",
     title: "QR Code Generator",
-    body: "Crea códigos QR al instante con esta herramienta sencilla y eficiente. Personaliza tu código con el contenido que necesites y descárgalo fácilmente en formato PNG para usarlo donde quieras.",
+    description:
+      "Crea códigos QR al instante con esta herramienta sencilla y eficiente. Personaliza tu código con el contenido que necesites y descárgalo fácilmente en formato PNG para usarlo donde quieras.",
     image: "/images/qrcode.webp",
     tools: ["ReactJS", "Tailwind"],
     deploy: "https://qrcode-generator2024.vercel.app/",
   },
   {
-    href: "",
+    github: "",
     title: "Active Rent a Car",
-    body: "Desarrollé este sitio web completo para Active Rent a Car, una plataforma de alquiler de automóviles. Incluye un sistema de calendario intuitivo para reservas y un panel de administración que permite agregar, modificar y eliminar vehículos de forma eficiente.",
+    description:
+      "Desarrollé este sitio web completo para Active Rent a Car, una plataforma de alquiler de automóviles. Incluye un sistema de calendario intuitivo para reservas y un panel de administración que permite agregar, modificar y eliminar vehículos de forma eficiente.",
     image: "/images/active.webp",
     tools: ["NextJS", "TypeScript", "CSS", "GoogleAPI"],
     deploy: "https://active-rentacar.vercel.app/",
   },
   {
-    href: "",
+    github: "",
     title: "123 Auto Rental",
-    body: "Desarrollé este sitio web completo para Active Rent a Car, una plataforma de alquiler de automóviles. Incluye un sistema de calendario intuitivo para reservas y un panel de administración que permite agregar, modificar y eliminar vehículos de forma eficiente.",
+    description:
+      "Desarrollé este sitio web completo para 123 Auto Rental, una plataforma de alquiler de automóviles. Incluye un sistema de calendario intuitivo para reservas y un panel de administración que permite agregar, modificar y eliminar vehículos de forma eficiente.",
     image: "/images/123auto.webp",
     tools: ["NextJS", "CSS", "GoogleAPI"],
     deploy: "https://123autorental.vercel.app/",
   },
   {
-    href: "https://github.com/hugok2k/cloudinary-hackaton",
+    github: "https://github.com/hugok2k/cloudinary-hackaton",
     title: "CustomAvatar",
-    body: "Proyecto desarrollado para la hackathon de Cloudinary, CustomAvatar utiliza inteligencia artificial de Cloudinary para detectar rostros y generar avatares personalizados. Una muestra de integración de IA para experiencias de usuario.",
+    description:
+      "Proyecto desarrollado para la hackathon de Cloudinary, CustomAvatar utiliza inteligencia artificial de Cloudinary para detectar rostros y generar avatares personalizados. Una muestra de integración de IA para experiencias de usuario.",
     image: "/images/customavatar.webp",
     tools: ["NextJS", "Tailwind"],
     deploy: "https://cloudinary-hackaton.vercel.app/",
   },
   {
-    href: "https://github.com/hugok2k/hacktoberfest-2022",
+    github: "https://github.com/hugok2k/hacktoberfest-2022",
     title: "Password Generator",
-    body: "Un generador de contraseñas aleatorias flexible y seguro, creado para Hacktoberfest. Permite generar contraseñas utilizando una combinación de letras, números, símbolos o incluso palabras, ofreciendo múltiples niveles de seguridad.",
+    description:
+      "Un generador de contraseñas aleatorias flexible y seguro, creado para Hacktoberfest. Permite generar contraseñas utilizando una combinación de letras, números, símbolos o incluso palabras, ofreciendo múltiples niveles de seguridad.",
     image: "/images/hacktoberfest2022.webp",
     tools: ["Astro", "ReactJS", "CSS"],
     deploy: "https://hacktoberfest2022.vercel.app/",
